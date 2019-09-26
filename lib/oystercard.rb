e000212c8462d53c164fb4ff9e6fcf5bb4114bf3
Brad Phillips <bjrp@protonmail.com>
2019-09-26 19:24:17 +0100

    Refactors to add on_journey variable. Adds journey class file

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,12 @@
 class Oystercard
-  attr_reader :balance, :journey_history
+  attr_reader :balance, :journey_history, :on_journey
   MAX_BALANCE = 90
   MIN_BALANCE = 1
 
   def initialize
     @balance = 0
     @journey_history = []
+    @on_journey = nil
   end
 
   def top_up(amount)
@@ -13,18 +14,21 @@ class Oystercard
 
     @balance += amount
   end
-
+    
   def in_journey?
-    !!@journey_history[-1].value?(:unfinished)
+    @on_journey
   end
 
   def touch_in(station)
     raise "Error: Cannot touch in, your balance is less than minimum balance £#{MIN_BALANCE}" if @balance < MIN_BALANCE
-
+    raise 'Already in journey' if in_journey?
+    @on_journey = true
     add_journey(station)
   end
 
   def touch_out(station)
+    raise 'not in journey' unless in_journey?
+    @on_journey = false
     add_journey(station)
     deduct(MIN_BALANCE)
   end
@@ -35,12 +39,12 @@ class Oystercard
     @balance -= amount
   end
 
-  def add_journey(station)
-    if @journey_history.empty? || !@journey_history[-1].value?(:unfinished)
-      @journey_history << { entry_station: station, exit_station: :unfinished }
-    else
-      @journey_history[-1][:exit_station] = station
+  # def add_journey(station)
+  #   if on_journey == true
+  #     @journey_history << { entry_station: station, exit_station: :unfinished }
+  #   else
+  #     @journey_history[-1][:exit_station] = station
 
-    end
-  end
+  #   end
+  # end
 end
